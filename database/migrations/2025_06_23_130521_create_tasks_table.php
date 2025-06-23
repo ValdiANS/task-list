51f867fc5cf1f8234f1ca7686704faaf02ecfdd1
Valdi <rvdans002@gmail.com>
2025-06-23 20:51:58 +0700

    create Task migration

---
+++ database/migrations/2025_06_23_130521_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\TaskPriority;
+use App\Enums\TaskStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description');
+            $table->text('long_description')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->enum('priority', array_column(TaskPriority::cases(), 'value'));
+            $table->enum('status', array_column(TaskStatus::cases(), 'value'));
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
