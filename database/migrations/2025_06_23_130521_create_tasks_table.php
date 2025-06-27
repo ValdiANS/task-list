fa73c8c99e88150dac41b074a6e3e32f12be107b
Valdi <rvdans002@gmail.com>
2025-06-27 13:25:20 +0700

    change primary key to UUID instead of int

--- database/migrations/2025_06_23_130521_create_tasks_table.php
+++ database/migrations/2025_06_23_130521_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
 
             $table->string('title');
             $table->text('description');
