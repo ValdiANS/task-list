1c62aaa538d9d075a377f94eb6e7c09650c09c57
Valdi <rvdans002@gmail.com>
2025-06-27 13:25:02 +0700

    add table name, primary key, default values, and fillable field to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $table = 'tasks';
+    protected $primaryKey = 'id';
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'long_description' => '',
+        'completed' => false,
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'long_description',
+        'status',
+        'priority',
+        'completed',
+    ];
 }
